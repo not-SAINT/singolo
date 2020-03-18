4b120152a8d039947471be1074fa0763c48e166e
notsaint <jedilexx@gmail.com>
2020-03-18 12:45:03 +0300

    feat: add menu highlight when scrolling

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 const selectItem = (event, className, arrToUnselect) => {
   arrToUnselect.forEach(el => el.classList.remove(className)); 
-  event.target.classList.add(className);
+  let target = event.target ? event.target : document.querySelector(`.navigation > li > a[href*=${event}]`);
+  target.classList.add(className);
 }
  
 // header
@@ -12,7 +13,32 @@ const menuSelected = (event) => {
   selectItem(event, 'active', menuElements);
 }
 
+let blocksPosition = [];
+
+const getBlocksPosition = () => {
+  const wrappers = document.querySelectorAll('.wrapper');
+
+  wrappers.forEach((el) => {
+    if (el.getAttribute('id'))
+      blocksPosition.push({id: el.getAttribute('id'), startY: el.offsetTop, endY: el.offsetTop + el.offsetHeight})         
+  })
+  blocksPosition[0].endY += 600;
+}
+
+getBlocksPosition();
+
+const onScroll = () => {
+    const activePos = window.scrollY; 
+
+    for (let i = 0; i < blocksPosition.length; i++)      
+      if (blocksPosition[i].startY - (blocksPosition[i].endY - blocksPosition[i].startY) / 2 <= activePos 
+          && blocksPosition[i].endY > activePos) {
+            selectItem(blocksPosition[i].id, 'active', menuElements);        
+          }
+  }
+
 menuElements.forEach(el => el.addEventListener('click', menuSelected));
+document.addEventListener('scroll', onScroll);
 
 // slider
 const SLIDER = document.querySelector('.slider');
@@ -108,15 +134,16 @@ const portfolioImageSelected = (event) => {
   selectItem(event, 'image_selected', portfolioImages);
 }
 
-const portfolioNav = document.querySelectorAll('.portfolio__link');
+const portfolioNav = document.querySelector('.portfolio__navigation');
+const portfolioLinks = document.querySelectorAll('.portfolio__link');
 const portfolioNavSelected = (event) => {
-  selectItem(event, 'portfolio__link_selected', portfolioNav);
+  selectItem(event, 'portfolio__link_selected', portfolioLinks);
 }
 
-const images = document.querySelectorAll('.portfolio__image');
-const imagesDefaultSrc = Array.prototype.slice.call(images).map( el => el.src);
+const imageContainer = document.querySelector('.layout-4-column');
 
-const setImageRandom = (event) => {
+const moveImageRandom = () => {
+  let images = document.querySelectorAll('.portfolio__image'); 
   let positionsSet = new Set(Array(12).fill(1).map((a,i) => i));
   let rndPosition = 0;
   let randomPositions =[];
@@ -129,38 +156,55 @@ const setImageRandom = (event) => {
     }
   }
 
+  imageContainer.innerHTML = '';
   for (let i = 0; i < images.length; i++)
-    images[i].src = imagesDefaultSrc[randomPositions[i]];    
+    imageContainer.append(images[randomPositions[i]])
 }
 
-const moveImageLeft = (event) => {
-  let first = images[0].src;
-  for (let i = 1; i <= images.length; i++)
-    images[i - 1].src = images[i % images.length].src;    
-  
-  images[images.length - 1].src = first;  
+const moveImageLeft = () => {
+  let images = document.querySelectorAll('.portfolio__image');
+  let first = imageContainer.removeChild(images[0]);
+  imageContainer.append(first);
 }
 
-const moveImageRight = (event) => {
-  let last = images[images.length - 1].src;
-  for (let i = images.length - 1; i > 0; i--)
-    images[i].src = images[i - 1].src;    
-  
-  images[0].src = last;  
+const moveImageRight = () => {
+  let images = document.querySelectorAll('.portfolio__image');
+  let last = imageContainer.removeChild(images[images.length - 1]);
+  imageContainer.prepend(last);
 }
 
-const setImageDefault = (event) => {
-  for (let i = 0; i < images.length; i++)
-    images[i].src = imagesDefaultSrc[i];  
+const moveTwoDirections = () => {
+  let images = document.querySelectorAll('.portfolio__image');
+  let left = imageContainer.removeChild(images[Math.trunc(images.length / 2) - 1]);
+  let right = imageContainer.removeChild(images[Math.trunc(images.length / 2)]);
+
+  imageContainer.prepend(left);
+  imageContainer.append(right); 
 }
 
-portfolioNav[0].addEventListener('click', setImageRandom);
-portfolioNav[1].addEventListener('click', moveImageLeft);
-portfolioNav[2].addEventListener('click', moveImageRight);
-portfolioNav[3].addEventListener('click', setImageDefault);
+const shuffleImages = (event) => {
+  switch (event.target.id) {
+    case 'all':
+      moveImageRandom();
+      portfolioNavSelected(event);
+      break;
+    case 'web-design':
+      moveImageLeft();
+      portfolioNavSelected(event);
+      break;
+    case 'graphic-design':
+      moveImageRight();
+      portfolioNavSelected(event);
+      break;
+    case 'artwork':
+      moveTwoDirections();
+      portfolioNavSelected(event);
+      break;
+  }    
+}
 
+portfolioNav.addEventListener('click', shuffleImages);
 portfolioImages.forEach(el => el.addEventListener('click', portfolioImageSelected));
-portfolioNav.forEach(el => el.addEventListener('click', portfolioNavSelected));
 
 // Get a Quote
 const OK_BUTTON = document.getElementById('ok-btn');
@@ -175,7 +219,6 @@ FORM.addEventListener('submit', (e) => {
 );
 
 const doModal = () => {
-
   const subject = document.getElementById('subject').value;
   const describe = document.getElementById('describe').value;
 
